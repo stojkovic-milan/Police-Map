b91f011d3e4e685e36415d94f257bee8175a4c8f
Milan Stojkovic <milanstojkovic2707@gmail.com>
2023-07-08 18:12:52 +0200

    feat: Places list styling completed

--- app/src/main/java/com/example/policemap/PlaceRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/policemap/PlaceRecyclerViewAdapter.kt
@@ -1,14 +1,23 @@
 package com.example.policemap
 
+import android.location.Geocoder
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.policemap.data.model.Place
+import com.example.policemap.data.model.PlaceType
 
 import com.example.policemap.databinding.FragmentPlaceListBinding
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.material.chip.Chip
+import com.google.firebase.database.*
 import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * [RecyclerView.Adapter] that can display a [PlaceholderItem].
@@ -17,8 +26,10 @@ import org.w3c.dom.Text
 class PlaceRecyclerViewAdapter(
     private val values: List<Place>
 ) : RecyclerView.Adapter<PlaceRecyclerViewAdapter.ViewHolder>() {
+    private lateinit var databaseRef: DatabaseReference
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        databaseRef = FirebaseDatabase.getInstance().getReference("users")
 
         return ViewHolder(
             FragmentPlaceListBinding.inflate(
@@ -33,14 +44,72 @@ class PlaceRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
         holder.typeView.text = item.placeType.toString()
-        holder.timeView.text = item.time.toString()
-        holder.expirationView.text = item.expirationTime.toString()
-        //TODO: Get users username instead of ID
-        holder.userView.text = item.userId.toString()
-        //TODO: Use google maps geocoding to display adress here
-        holder.addressView.text = "Address goes here"
-        holder.ratingView.text = item.rating.toString()
+        val pattern = "HH:mm:ss  dd.MM.yyyy"
+        val simpleDateFormat = SimpleDateFormat(pattern)
+        holder.timeView.text = "Reported: %s".format(simpleDateFormat.format(item.time))
+        var expired = false
+        if (item.expirationTime!! < Date()) {
+            expired = true
+            holder.expiredChip.visibility = View.VISIBLE
+        }
+        holder.expirationView.text =
+            (if (expired) "Expired: %s" else "Expires: %s").format(simpleDateFormat.format((item.expirationTime)))
+        val userRef = databaseRef.child(item.userId!!)
 
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Check if the user node exists and has children
+                if (dataSnapshot.exists() && dataSnapshot.hasChildren()) {
+                    // Retrieve the username value from the dataSnapshot
+                    val username = dataSnapshot.child("username").getValue(String::class.java)
+
+                    // Check if the username is not null
+                    if (username != null) {
+                        // Update the user view with the retrieved username value
+                        holder.userView.text = "Reported by: %s".format(username)
+                    } else {
+                        // Fallback to displaying the userId if the username is null
+                        holder.userView.text = "Reported by: %s".format(item.userId)
+                    }
+                } else {
+                    // Fallback to displaying the userId if the user node doesn't exist or has no children
+                    holder.userView.text = item.userId
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle database error
+                holder.userView.text = item.userId
+            }
+        })
+
+
+        val geocoder = Geocoder(holder.addressView.context, Locale.getDefault())
+
+        val addressList = geocoder.getFromLocation(
+            item.latLng?.latitude!!,
+            item.latLng.longitude!!,
+            1
+        )
+        if (addressList?.isNotEmpty() == true)
+            holder.addressView.text = addressList[0].getAddressLine(0)
+        else
+            holder.addressView.text = "Unknown Address"
+
+        holder.ratingView.text = "Rating: %d".format(item.rating)
+
+        val drawable = ContextCompat.getDrawable(
+            holder.iconView.context,
+            when (item.placeType) {
+                PlaceType.Radar -> R.drawable.police_radar_32
+                PlaceType.Control -> R.drawable.police_stop_32
+                PlaceType.Camera -> R.drawable.police_camera_32
+                else -> R.drawable.police_patrol_32
+            }
+        )
+
+
+        holder.iconView.setImageDrawable(drawable)
     }
 
     override fun getItemCount(): Int = values.size
@@ -53,6 +122,8 @@ class PlaceRecyclerViewAdapter(
         val expirationView: TextView = binding.placeExpiration
         val userView: TextView = binding.placeUser
         val ratingView: TextView = binding.placeRating
+        val iconView: ImageView = binding.reportIcon
+        val expiredChip: Chip = binding.chipExpired
 
 //        val idView: TextView = binding.itemNumber
 //        val contentView: TextView = binding.content
