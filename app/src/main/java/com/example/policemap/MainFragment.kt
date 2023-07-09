64755c181f9d1f562d3c6ab444b51efe84ae8294
Milan Stojkovic <milanstojkovic2707@gmail.com>
2023-07-10 01:01:52 +0200

    feat: app icons added, login, register and main page finished

--- app/src/main/java/com/example/policemap/MainFragment.kt
+++ app/src/main/java/com/example/policemap/MainFragment.kt
@@ -21,8 +21,6 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
